a301b95f5658ad797d9ffb3dee00c0c193adf1f1
Meghan Flynn <mflynn333@gmail.com>
2020-01-14 17:57:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,48 @@ class Song
   def save
     self.class.all << self
   end
+  
+ def self.create 
+   song = self.new  
+   @@all << song 
+   song 
+ end 
+  
+  def self.new_by_name(name) 
+    song = self.new 
+    song.name = name 
+    song
+  end 
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    @@all << song 
+    song
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name) 
+    song = self.find_by_name(name)
+    if song 
+      return song 
+    else 
+      self.create_by_name(name)
+    end 
+  end 
+  
+  def self.alphabetical 
+    @@all.sort_by { |song_name| song_name.name}
+  end 
+  
+  def self.new_from_filename 
+  end 
+  
+  def self.destroy_all
+    self.all.clear
+  end 
 
 end
