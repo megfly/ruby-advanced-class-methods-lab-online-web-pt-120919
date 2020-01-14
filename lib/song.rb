ad6a29cd85c69ec24a0dc1b44b4e2b286dc769d3
Meghan Flynn <mflynn333@gmail.com>
2020-01-14 18:44:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
   end 
   
   def self.create_from_filename(filename) 
-    split_filename = filename.chomp(".mp3")
+    split_filename = filename.chop.chop.chop.chop.split(" - ")
     song = Song.new 
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
