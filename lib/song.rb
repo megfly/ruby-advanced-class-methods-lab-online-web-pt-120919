46c407de60bea0c405553608332b0a9c05314fb5
Meghan Flynn <mflynn333@gmail.com>
2020-01-14 18:35:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   end 
   
   def self.new_from_filename(filename) 
-    split_filename = filename.chomp
+    split_filename = filename.chomp(".mp3")
   end 
   
   def self.destroy_all
